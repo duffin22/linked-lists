cfa9153694c49a3281269698db49566838ce5c09
Matthew Duffin <matthewtduffin@gmail.com>
2016-08-24 10:29:11 -0700

    init commit

---
+++ src/LinkedList.java
@@ -0,0 +1,24 @@
+/**
+ * Created by matthewtduffin on 24/08/2016.
+ */
+public class LinkedList {
+
+  private Node head;
+  private int size = 0;
+
+  public int getSize() {
+    return size;
+  }
+
+  public void add(Object obj) {
+
+  }
+
+  public boolean remove(int index) {
+    
+  }
+
+  public Object get(int index) {
+
+  }
+}
---
+++ src/Node.java
@@ -0,0 +1,32 @@
+/**
+ * Created by matthewtduffin on 24/08/2016.
+ */
+public class Node {
+  Node next;
+  Object data;
+
+  public Node(Object data) {
+    this(data, null);
+  }
+
+  public Node(Object data, Node next) {
+    this.next = next;
+    this.data = data;
+  }
+
+  public Object getData() {
+    return this.data;
+  }
+
+  public void setData(Object data) {
+    this.data = data;
+  }
+
+  public Node getNext() {
+    return this.next;
+  }
+
+  public void setNext(Node nextNode) {
+    this.next = nextNode;
+  }
+}
